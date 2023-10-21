20dad54ad55862ab261d3b4ea8c353bcb82c8f91
Stephen Mulligan <smulligan@pp.studio>
2023-10-21 16:15:02 +0200

    Initial structure of main and config files

---
+++ main.py
@@ -0,0 +1,23 @@
+import os
+import argparse
+import config
+
+def main(args):
+    #### Unpack arguments ####
+    mode = args.mode
+    name = args.name
+
+    if mode == 'train':
+        params = config.train_params
+    elif mode =='test':
+        params = config.test_params
+    else:
+        print('Select valid mode: train or test')
+        exit()
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--mode', type=str, default='train')
+    parser.add_argument('--name', type=str, default='test_run')
+    args = parser.parse_args()
+    main(args)
